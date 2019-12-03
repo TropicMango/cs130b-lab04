8faa17225410ee3333f778cfcec9e3450eaf21d4
Charlie Wan <cwan@umail.ucsb.edu>
2019-12-03 02:34:51 -0800

    regession change

--- RandomizedFitting.cpp
+++ RandomizedFitting.cpp
@@ -20,7 +20,7 @@ void RandomizedFitting::fit(vector<Point> p_list){
     float min_error = -1;
     Point min_p1, min_p2;
     int num_points = p_list.size();
-    for(int i=0; i<max(5000, num_points); i++){
+    for(int i=0; i<max(10000, num_points); i++){
         // generate two random unique index
         int index_1 = rand() % num_points;
         int index_2 = rand() % num_points;
@@ -57,32 +57,21 @@ void RandomizedFitting::fit(vector<Point> p_list){
     // float m = (min_p2.y-min_p1.y) / (min_p2.x-min_p1.x);
     // float b = min_p1.y - m*min_p1.x;
     
-
-    vector<Point> fit_points;
-    float avg_x, avg_y;
+    float sum_x=0, sum_y=0, sum_xy=0, sum_xx=0, num_fit=0;
     // finding the points to perform regression
     for(int i=0; i<num_points; i++){
         Point p_temp = p_list[i];
         if(DistFromLine(min_p1, min_p2, p_temp) < min_error){
-            fit_points.push_back(p_temp);
-            avg_x += p_temp.x;
-            avg_y += p_temp.y;
+            sum_x += p_temp.x;
+            sum_y += p_temp.y;
+            sum_xy += p_temp.x * p_temp.y;
+            sum_xx += p_temp.x * p_temp.x;
+            num_fit ++;
         }
     }
 
-    avg_x /= fit_points.size();
-    avg_y /= fit_points.size();
-
-    float numerator = 0;
-    float denominator = 0;
-    // applying regression to find slope
-    for(int i=0; i<fit_points.size(); i++){
-        Point p_temp = fit_points[i];
-        numerator += (p_temp.x-avg_x) * (p_temp.y-avg_y);
-        denominator += (p_temp.x-avg_x) * (p_temp.x-avg_x);
-    }
-    float a = numerator/denominator;
-    float b = avg_y - a * avg_x;
+    float a = (num_fit * sum_xy - sum_x * sum_y) / (num_fit * sum_xx - sum_x * sum_x);
+    float b = (sum_y - a * sum_x) / num_fit;
 
     cout << fixed;
     cout << setprecision(5);
